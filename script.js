36c571fafb73309d35eefbfd96babfeb523ca3e9
Jeremy M <Jinoshi@IT-020537.local>
2022-11-28 12:52:55 -0600

    Added JS and Other stuff

--- script.js
+++ script.js
@@ -5,12 +5,32 @@ const totalSlides = slides.length
 document.getElementById("carousel-button-next").addEventListener("click", moveToNextSlide)
 document.getElementById("carousel-button-prev").addEventListener("click", moveToPreviusSlide)
 
+function hideAllSlides() {
+	for (let slide of slides) {
+		slide.classList.remove("carousel-item-visible")
+		slide.classList.add("carousel-item-hidden")
+	}
+}
+
+
 function moveToNextSlide() {
-	console.log("Next slide!")
+	hideAllSlides()
+	if (slidePosition === totalSlides - 1) {
+		slidePosition = 0
+	} else{
+		slidePosition++
+	}
+	slides[slidePosition].classList.add("carousel-item-visible")
 }
 
 function moveToPreviusSlide() {
-	console.log("Previus slide!")
+	hideAllSlides()
+	if (slidePosition === 0){
+		slidePosition = totalSlides -1
+	} else {
+		slidePosition--
+	}
+	slides[slidePosition].classList.add("carousel-item-visible")
 }
 
 
